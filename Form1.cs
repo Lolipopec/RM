7f26dd9e797480ce3c8236b43e9c2d90469e6bbd
Lolipop <pacha_vlasov_95@mail.ru>
2021-06-23 22:21:12 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
+using System.Diagnostics;
+
+namespace Redmine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("Login");
+                var host = "http://testred.ru";
+                var login = "user1";
+                var password = "12345678";
+                var a = new RedmineManager(host, login, password);
+                User currentUser = a.GetCurrentUser();
+                Debug.WriteLine("Current user: {0}.", currentUser.FirstName);
+                Menu m = new Menu(login, password);
+                m.Show();
+                this.Hide();
+            }
+            catch 
+            { 
+                MessageBox.Show("Неправильный логин или пароль."); 
+            }
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,49 @@
+﻿using Redmine.Net.Api;
+using Redmine.Net.Api.Async;
+using Redmine.Net.Api.Types;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Redmine
+{
+    public partial class Menu : Form
+    {
+        public static string host = "http://testred.ru";
+        public static string login = "";
+        public static string password = "";
+        public Menu(string login, string password)
+        {
+            InitializeComponent();
+            Redmine.Menu.login = login;
+            Redmine.Menu.password = password;
+            var manager = new RedmineManager(host, login, password);
+
+            var parameters = new NameValueCollection {  };
+            parameters.Add(RedmineKeys.STATUS_ID, "open");
+            parameters.Add(RedmineKeys.ASSIGNED_TO_ID, "me");
+            foreach (var issue in manager.GetObjects<Issue>(parameters))
+            {
+                int i = 0;
+                Debug.WriteLine(issue.Id);
+                dataGridView1.Rows[i].Cells[0].Value = issue.Id;
+                dataGridView1.Rows[i].Cells[1].Value = issue.Subject;
+                dataGridView1.Rows[i].Cells[2].Value = issue.AssignedTo.Name;
+                i++;
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
